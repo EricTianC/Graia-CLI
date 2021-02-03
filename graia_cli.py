35adf2db249b58698305a18e89851f2bcd2424f4
EricTianC <49602673+EricTianC@users.noreply.github.com>
2021-02-03 13:50:58 +0800

    Update graia_cli.py
    加了空行

--- graia_cli.py
+++ graia_cli.py
@@ -1,15 +1,25 @@
 import typer
 import sys
 import os
+
+
 def good_echo(text):
     typer.echo(typer.style(text,fg=typer.colors.BRIGHT_GREEN,bold=True))
+
+
 def bad_echo(text):
     typer.echo(typer.style(text,fg=typer.colors.RED,bold=True))
+
+
 def normal_echo(text):
     typer.echo(typer.style(text,fg=typer.colors.WHITE,bold=True))
+
+
 class graia_installer():
+    
     def __init__(self):
         self.py_path=sys.executable
+        
     def install(self):
         good_echo("开始安装Graia")
         commands = f"{self.py_path} -m pip install graia-application-mirai"
@@ -19,6 +29,7 @@ class graia_installer():
             good_echo("安装成功，前往Github查看文档")
         except:
             bad_echo("安装失败")
+            
     def upgrade(self):
         os.system(f"{self.py_path} -m pip install graia-application-mirai -U")
         try:                
@@ -26,11 +37,14 @@ class graia_installer():
             good_echo("更新成功")
         except:                                         
             bad_echo("更新失败")
+            
     def install_v(self,v):
         os.system(f"{self.py_path} -m pip install graia-application-mirai=={v}")
-def main(install: bool=False,installv:str= typer.Argument(""),
-        upgrade: bool=False
-):
+        
+        
+def main(install: bool=False,
+         installv:str= typer.Argument(""),
+         upgrade: bool=False):
     """ 
     ./graia_cli --install 安装Graia  可选参数版本号
     
@@ -43,6 +57,7 @@ def main(install: bool=False,installv:str= typer.Argument(""),
         installer.install()
     if installv != "":
         installer.install_v(installv)
+        
         
 if __name__ == "__main__":
         typer.run(main)
